a01f344f62c2133ff576ed13b267d02d3b265317
Gabriel Wu <qqbbnease1004@126.com>
2018-04-18 21:23:58 +0800

    add const.go
    using iota

---
+++ basic/const.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const (
+	Sunday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+func main() {
+	fmt.Println(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
+}
